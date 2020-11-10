84ac55f6393cef117a862a4143fac37960d2d2a4
Billy Cook <cook0318@algonquinlive.com>
2020-11-10 15:59:34 -0500

    Edited Test file

--- PhpProject/Test.php
+++ PhpProject/Test.php
@@ -1,4 +1,16 @@
-<?php
+<html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>Test Test</title>
+    </head>
 
-print "Testing testing";
-?>
+    <body>
+
+        <?php
+        print "<h1>If you can see this, we good</h1>";
+        ?> 
+
+    </body>
+</html>
+
+
