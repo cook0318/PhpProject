3fe46380cef8e1745d28356644f4b664d5f7c7d0
evertonvolpi <everton.volpi@gmail.com>
2020-11-30 16:49:28 -0500

    Header.php updated

--- PhpProject/Common/Header.php
+++ PhpProject/Common/Header.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en" style="position: relative; min-height: 100%;">
 <head>
@@ -7,32 +9,28 @@
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="../CSS/Site.css" rel="stylesheet" type="text/css"/>
 </head>
-<body style="padding-top: 50px; margin-bottom: 60px;">
-    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" style="padding: 10px" href="http://www.algonquincollege.com">
-                    <img src="/AlgCommon/Contents/img/AC.png"
-                    alt="Algonquin College" style="max-width:100%; max-height:100%;"/>
-                </a>
-            </div>
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li class="<?php if($activePage == "Index") { echo "active disableLink"; } ?>"><a href="Index.php">Home</a></li>
-                    <li class="<?php if($activePage == "MyFriends") { echo "active disableLink"; } ?>"><a href="MyFriends.php">My Friends</a></li>
-                    <li class="<?php if($activePage == "MyAlbums") { echo "active disableLink"; } ?>"><a href="MyAlbums.php">My Albums</a></li>
-                    <li class="<?php if($activePage == "MyPictures") { echo "active disableLink"; } ?>"><a href="MyPictures.php">My Pictures</a></li>
-                    <li class="<?php if($activePage == "UploadPictures") { echo "active disableLink"; } ?>"><a href="UploadPictures.php">Upload Pictures</a></li>
-                    <li class="<?php if($activePage == "Login") { echo "active disableLink"; } ?>"><a href="Login.php">Log In</a></li>
-                </ul>
-            </div>
+<body style="margin-bottom: 60px;">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-top">
+        <a class="navbar-brand" style="padding: 10px" href="http://www.algonquincollege.com">
+            <img src="/AlgCommon/Contents/img/AC.png"
+            alt="Algonquin College" style="max-width:100%; max-height:100%;"/>
+        </a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto"> <!-- was just nav before-->
+
+            
+                <li class="nav-item <?php if($activePage == "Index") { echo "active disableLink"; } ?>"><a class="nav-link" href="Index.php">Home</a></li>
+                <li class="nav-item <?php if($activePage == "MyFriends") { echo "active disableLink"; } ?>"><a class="nav-link" href="MyFriends.php">My Friends</a></li>
+                <li class="nav-item <?php if($activePage == "MyAlbums") { echo "active disableLink"; } ?>"><a class="nav-link" href="MyAlbums.php">My Albums</a></li>
+                <li class="nav-item <?php if($activePage == "MyPictures") { echo "active disableLink"; } ?>"><a class="nav-link" href="MyPictures.php">My Pictures</a></li>
+                <li class="nav-item <?php if($activePage == "UploadPictures") { echo "active disableLink"; } ?>"><a class="nav-link" href="UploadPictures.php">Upload Pictures</a></li>
+                <li class="nav-item <?php if($activePage == "Login") { echo "active disableLink"; } ?>"><a class="nav-link" href="Login.php">Log In</a></li>
+            
+            </ul>
         </div>
     </nav>
